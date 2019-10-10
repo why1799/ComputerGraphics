1dbafd95c6cd3bcae0d2ca9af163df0e2951545d
Харченко Артём <why1799@gmail.com>
2019-10-10 20:22:42 +0300

    D&D

--- ComputerGraphics/Проект 3. Безье/MyForm.h
+++ ComputerGraphics/Проект 3. Безье/MyForm.h
@@ -19,12 +19,21 @@ namespace Bez {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 
-	public:
+	private:
 		List<MyPoint^>^ points;
 		bool add;
-	private: System::Windows::Forms::RadioButton^ radioButton3;
-	public:
+		
 		bool end;
+		int rex = 5;
+		int rin = 2;
+
+		float draganddropaddx;
+		float draganddropaddy;
+		int draganddropitem;
+		bool dad;
+		float mousex;
+	private: System::Windows::Forms::CheckBox^ checkBox5;
+		   float mousey;
 
 	public:
 		MyForm(void)
@@ -38,6 +47,7 @@ namespace Bez {
 			panel3->Enabled = false;
 			add = false;
 			end = false;
+			dad = false;
 		}
 
 	protected:
@@ -66,6 +76,7 @@ namespace Bez {
 	private: System::Windows::Forms::CheckBox^ checkBox2;
 	private: System::Windows::Forms::CheckBox^ checkBox3;
 	private: System::Windows::Forms::CheckBox^ checkBox4;
+	private: System::Windows::Forms::RadioButton^ radioButton3;;
 
 
 
@@ -88,6 +99,7 @@ namespace Bez {
 			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
 			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
 			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
@@ -97,7 +109,7 @@ namespace Bez {
 			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
 			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
 			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
-			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
+			this->checkBox5 = (gcnew System::Windows::Forms::CheckBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->panel1->SuspendLayout();
 			this->panel2->SuspendLayout();
@@ -116,6 +128,8 @@ namespace Bez {
 			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
 			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::pictureBox1_Paint);
 			this->pictureBox1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseDown);
+			this->pictureBox1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseMove);
+			this->pictureBox1->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseUp);
 			// 
 			// radioButton1
 			// 
@@ -155,16 +169,30 @@ namespace Bez {
 			this->panel1->Size = System::Drawing::Size(233, 111);
 			this->panel1->TabIndex = 3;
 			// 
+			// radioButton3
+			// 
+			this->radioButton3->AutoSize = true;
+			this->radioButton3->Location = System::Drawing::Point(4, 74);
+			this->radioButton3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			this->radioButton3->Name = L"radioButton3";
+			this->radioButton3->Size = System::Drawing::Size(104, 24);
+			this->radioButton3->TabIndex = 3;
+			this->radioButton3->TabStop = true;
+			this->radioButton3->Text = L"B-сплайн";
+			this->radioButton3->UseVisualStyleBackColor = true;
+			this->radioButton3->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton3_CheckedChanged);
+			// 
 			// panel2
 			// 
 			this->panel2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->panel2->Controls->Add(this->checkBox5);
 			this->panel2->Controls->Add(this->checkBox1);
 			this->panel2->Controls->Add(this->button2);
 			this->panel2->Controls->Add(this->button1);
 			this->panel2->Location = System::Drawing::Point(18, 152);
 			this->panel2->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(233, 130);
+			this->panel2->Size = System::Drawing::Size(233, 192);
 			this->panel2->TabIndex = 4;
 			// 
 			// checkBox1
@@ -203,7 +231,7 @@ namespace Bez {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(18, 369);
+			this->button3->Location = System::Drawing::Point(18, 418);
 			this->button3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(112, 35);
@@ -216,7 +244,7 @@ namespace Bez {
 			// 
 			this->panel3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->panel3->Controls->Add(this->checkBox3);
-			this->panel3->Location = System::Drawing::Point(18, 305);
+			this->panel3->Location = System::Drawing::Point(18, 354);
 			this->panel3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->panel3->Name = L"panel3";
 			this->panel3->Size = System::Drawing::Size(233, 54);
@@ -237,7 +265,7 @@ namespace Bez {
 			// checkBox2
 			// 
 			this->checkBox2->AutoSize = true;
-			this->checkBox2->Location = System::Drawing::Point(18, 414);
+			this->checkBox2->Location = System::Drawing::Point(18, 463);
 			this->checkBox2->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->checkBox2->Name = L"checkBox2";
 			this->checkBox2->Size = System::Drawing::Size(266, 24);
@@ -249,7 +277,7 @@ namespace Bez {
 			// checkBox4
 			// 
 			this->checkBox4->AutoSize = true;
-			this->checkBox4->Location = System::Drawing::Point(18, 449);
+			this->checkBox4->Location = System::Drawing::Point(18, 498);
 			this->checkBox4->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->checkBox4->Name = L"checkBox4";
 			this->checkBox4->Size = System::Drawing::Size(235, 64);
@@ -258,18 +286,17 @@ namespace Bez {
 			this->checkBox4->UseVisualStyleBackColor = true;
 			this->checkBox4->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox4_CheckedChanged);
 			// 
-			// radioButton3
+			// checkBox5
 			// 
-			this->radioButton3->AutoSize = true;
-			this->radioButton3->Location = System::Drawing::Point(4, 74);
-			this->radioButton3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
-			this->radioButton3->Name = L"radioButton3";
-			this->radioButton3->Size = System::Drawing::Size(104, 24);
-			this->radioButton3->TabIndex = 3;
-			this->radioButton3->TabStop = true;
-			this->radioButton3->Text = L"B-сплайн";
-			this->radioButton3->UseVisualStyleBackColor = true;
-			this->radioButton3->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton3_CheckedChanged);
+			this->checkBox5->AutoSize = true;
+			this->checkBox5->Location = System::Drawing::Point(4, 128);
+			this->checkBox5->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			this->checkBox5->Name = L"checkBox5";
+			this->checkBox5->Size = System::Drawing::Size(219, 44);
+			this->checkBox5->TabIndex = 8;
+			this->checkBox5->Text = L"Прямая на опорных точ-\r\nках";
+			this->checkBox5->UseVisualStyleBackColor = true;
+			this->checkBox5->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox5_CheckedChanged);
 			// 
 			// MyForm
 			// 
@@ -299,36 +326,54 @@ namespace Bez {
 		}
 #pragma endregion
 	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
-		System::Windows::Forms::MouseEventArgs^ newe = (System::Windows::Forms::MouseEventArgs^)e;
+		
 
-		if (radioButton1->Checked)
-		{
-			if (points->Count >= 4 && (points->Count == 4 || (points->Count - 4) % 3 == 0))
-			{
-				return;
-			}
-		}
-
-		if (checkBox3->Checked && points->Count > 1)
-		{
-			points->Insert(points->Count - 1, gcnew MyPoint(newe->Location.X, newe->Location.Y));
-		}
-		else
-		{
-			points->Add(gcnew MyPoint(newe->Location.X, newe->Location.Y));
-		}
-		pictureBox1->Refresh();
+		
 	}
-	private: System::Void pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	}
+	
 	private: System::Void pictureBox1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
 
 
 		Color^ c = gcnew Color();
 		Pen^ pen = gcnew Pen(c->Red, 1);
 		SolidBrush^ brush = gcnew SolidBrush(c->Red);
-		int rex = 5;
-		int rin = 2;
+		
+		pen->Color = c->LightGray;
+		brush->Color = c->LightGray;
+
+		if (checkBox5->Checked && points->Count >= 4)
+		{
+			for (int i = 0; i < points->Count; i++)
+			{
+				if (i % 3 == 0)
+				{
+					float x1, y1, x2, y2;
+					if (i == 0 && end)
+					{
+						x1 = points[points->Count - 2]->X;
+						y1 = points[points->Count - 2]->Y;
+
+						x2 = points[1]->X;
+						y2 = points[1]->Y;
+					}
+					else
+					{
+						if (!(i + 1 < points->Count) || i == 0)
+							continue;
+
+						x1 = points[i - 1]->X;
+						y1 = points[i - 1]->Y;
+
+						x2 = points[i + 1]->X;
+						y2 = points[i + 1]->Y;
+					}
+					e->Graphics->DrawLine(pen, x1, y1, x2, y2);
+				}
+			}
+		}
+
+		pen->Color = c->Red;
+		brush->Color = c->Red;
 
 		if (checkBox1->Checked)
 		{
@@ -345,17 +390,35 @@ namespace Bez {
 					pen->Color = c->Green;
 					brush->Color = c->Green;
 				}
+
+				if (i == draganddropitem && dad)
+				{
+					pen->Color = c->Blue;
+					brush->Color = c->Blue;
+				}
 				e->Graphics->DrawEllipse(pen, points[i]->X - rex, points[i]->Y - rex, 2 * (float)rex, 2 * (float)rex);
 				e->Graphics->FillEllipse(brush, points[i]->X - rin, points[i]->Y - rin, 2 * (float)rin, 2 * (float)rin);
 			}
 		}
 		else
 		{
+			int i = 0;
 			//Отрисовка опорных точек
 			for each (auto p in points)
 			{
-				e->Graphics->DrawEllipse(pen, p->X - rex, p->Y - rex, 2 * (float)rex, 2 * (float)rex);
-				e->Graphics->FillEllipse(brush, p->X - rin, p->Y - rin, 2 * (float)rin, 2 * (float)rin);
+				if (i == draganddropitem && dad)
+				{
+					pen->Color = c->Blue;
+					brush->Color = c->Blue;
+				}
+					e->Graphics->DrawEllipse(pen, p->X - rex, p->Y - rex, 2 * (float)rex, 2 * (float)rex);
+					e->Graphics->FillEllipse(brush, p->X - rin, p->Y - rin, 2 * (float)rin, 2 * (float)rin);
+				if (i == draganddropitem && dad)
+				{
+					pen->Color = c->Red;
+					brush->Color = c->Red;
+				}
+				i++;
 			}
 		}
 
@@ -640,6 +703,7 @@ namespace Bez {
 			panel3->Enabled = true;
 			panel2->Enabled = false;
 			checkBox1->Checked = false;
+			checkBox5->Checked = false;
 			end = false;
 		}
 	}
@@ -650,6 +714,7 @@ namespace Bez {
 			panel3->Enabled = true;
 			panel2->Enabled = false;
 			checkBox1->Checked = false;
+			checkBox5->Checked = false;
 			end = false;
 		}
 	}
@@ -703,5 +768,153 @@ namespace Bez {
 		pictureBox1->Refresh();
 	}
 
-	};
+	private: System::Void pictureBox1_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		if (add)
+		{
+			if (radioButton1->Checked)
+			{
+				if (points->Count >= 4 && (points->Count == 4 || (points->Count - 4) % 3 == 0))
+				{
+					return;
+				}
+			}
+
+			if (checkBox3->Checked && points->Count > 1)
+			{
+				points->Insert(points->Count - 1, gcnew MyPoint(e->Location.X, e->Location.Y));
+			}
+			else
+			{
+				points->Add(gcnew MyPoint(e->Location.X, e->Location.Y));
+			}
+		}
+		else if(dad)
+		{
+			dad = false;
+		}
+
+		pictureBox1->Refresh();
+	}
+	private: System::Void pictureBox1_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		add = false;
+		if (dad)
+		{
+			mousex = e->Location.X;
+			mousey = e->Location.Y;
+
+			if (radioButton1->Checked)
+			{
+				float oldx = points[draganddropitem]->X;
+				float oldy = points[draganddropitem]->Y;
+				if (draganddropitem % 3 == 0)
+				{
+					points[draganddropitem]->X = mousex + draganddropaddx;
+					points[draganddropitem]->Y = mousey + draganddropaddy;
+					float changex = points[draganddropitem]->X - oldx;
+					float changey = points[draganddropitem]->Y - oldy;
+					if ((draganddropitem == 0 || draganddropitem == points->Count - 1) && end)
+					{
+						points[1]->X += changex;
+						points[1]->Y += changey;
+
+						points[points->Count - 2]->X += changex;
+						points[points->Count - 2]->Y += changey;
+					}
+					else
+					{
+						if (draganddropitem + 1 < points->Count && draganddropitem != 0)
+						{
+							points[draganddropitem - 1]->X += changex;
+							points[draganddropitem - 1]->Y += changey;
+
+							points[draganddropitem + 1]->X += changex;
+							points[draganddropitem + 1]->Y += changey;
+						}
+					}
+				}
+				if (draganddropitem % 3 == 1)
+				{
+					points[draganddropitem]->X = mousex + draganddropaddx;
+					points[draganddropitem]->Y = mousey + draganddropaddy;
+					float changex = points[draganddropitem]->X - oldx;
+					float changey = points[draganddropitem]->Y - oldy;
+					if (draganddropitem == 1 && end)
+					{
+						points[points->Count - 2]->X -= changex;
+						points[points->Count - 2]->Y -= changey;
+					}
+					else
+					{
+						if (draganddropitem != 1)
+						{
+							points[draganddropitem - 2]->X -= changex;
+							points[draganddropitem - 2]->Y -= changey;
+						}
+					}
+				}
+				if (draganddropitem % 3 == 2)
+				{
+					points[draganddropitem]->X = mousex + draganddropaddx;
+					points[draganddropitem]->Y = mousey + draganddropaddy;
+					float changex = points[draganddropitem]->X - oldx;
+					float changey = points[draganddropitem]->Y - oldy;
+					if (draganddropitem == points->Count - 2 && end)
+					{
+						points[1]->X -= changex;
+						points[1]->Y -= changey;
+					}
+					else
+					{
+						if (draganddropitem + 2 < points->Count && draganddropitem != points->Count - 2)
+						{
+							points[draganddropitem + 2]->X -= changex;
+							points[draganddropitem + 2]->Y -= changey;
+						}
+					}
+				}
+
+			}
+			else
+			{
+				points[draganddropitem]->X = mousex + draganddropaddx;
+				points[draganddropitem]->Y = mousey + draganddropaddy;
+			}
+			
+
+			if (checkBox3->Checked && (draganddropitem == 0 || draganddropitem == points->Count - 1))
+			{
+				points[points->Count - 1]->X = points[0]->X = mousex + draganddropaddx;
+				points[points->Count - 1]->Y = points[0]->Y = mousey + draganddropaddy;
+			}
+		}
+
+		
+
+		
+
+		pictureBox1->Refresh();
+	}
+	private: System::Void pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		add = true;
+
+		int i = 0;
+		for each(auto point in points)
+		{
+			if (Math::Sqrt((point->X - e->Location.X) * (point->X - e->Location.X) + (point->Y - e->Location.Y) * (point->Y - e->Location.Y)) <= rex)
+			{
+				add = false;
+				dad = true;
+				draganddropaddx = point->X - e->Location.X;
+				draganddropaddy = point->Y - e->Location.Y;
+				draganddropitem = i;
+				pictureBox1->Refresh();
+				return;
+			}
+			i++;
+		}
+	}
+private: System::Void checkBox5_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	pictureBox1->Refresh();
+}
+};
 }
